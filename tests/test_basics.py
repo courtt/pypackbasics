ab8b8aad57854bef12024cde19a2a3e1ec340f3b
Travis Court <travis@fq-wifi-10-215-208-33.wireless.pitt.edu>
2022-07-13 14:51:15 -0400

    added test

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,6 +1,7 @@
 import unittest
 
 import pypackbasics
+from pypackbasics.utilpack.utils import check_divisibility
 
 
 class TestPrimeFinder(unittest.TestCase):
@@ -9,3 +10,11 @@ class TestPrimeFinder(unittest.TestCase):
         finder.find_primes(max_prime=10)
         assert finder.known_primes == [2, 3, 5, 7]
         assert finder.highest_check == 10
+
+    def test_divisibility(self):
+        value = 32
+        divisors = [2,4,8,16]
+        assert pypackbasics.utilpack.check_divisibility(value,divisors)
+        divisors = [3]
+        assert pypackbasics.utilpack.check_divisibility(value,divisors)
+
