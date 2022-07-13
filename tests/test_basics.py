0acd5401d3bce1cfc43f16d63c861e3190dbd3b1
AlanPearl <alannpearl@outlook.com>
2022-07-13 10:32:03 -0400

    Added one test case

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -7,6 +7,5 @@ class TestPrimeFinder(unittest.TestCase):
     def test_find_primes_up_to_10(self):
         finder = pypackbasics.PrimeFinder()
         finder.find_primes(max_prime=10)
-        print(finder.known_primes)
         assert finder.known_primes == [2, 3, 5, 7]
         assert finder.highest_check == 10
